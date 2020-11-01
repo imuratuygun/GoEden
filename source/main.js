948d2b0f632cc796b5a086138382149b211d2334
imuratuygun <73760440+imuratuygun@users.noreply.github.com>
2020-11-01 20:07:35 +0300

    Update main.js

--- source/main.js
+++ source/main.js
@@ -1,7 +1,7 @@
 
 documnet.getElementById("heart").addEventListener("pointerup", function(event){
   navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
-.then(device => { /* … */ })
-.catch(error => { console.error(error); });
+  .then(device => { /* … */ })
+  .catch(error => { console.error(error); });
   
 });
